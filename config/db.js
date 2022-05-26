e568aae054607f21e91621bce4254a271ec37e1b
root <root@DESKTOP-DMI5NAQ.localdomain>
2022-05-25 21:14:33 -0500

    cambios en DB

--- config/db.js
+++ config/db.js
@@ -1,9 +1,9 @@
 const { Sequelize} = require('sequelize');
 
 const sequelize = new Sequelize(
-    'BeduShop',// Nombre de la DB
-    'root', // Usuario
-    '*9ak/oVTwtY_eI:.', //Contraseña
+    DATABASE,// Nombre de la DB
+    USER, // Usuario
+    PASSWORD, //Contraseña
     {
         host: '34.221.160.76', //host
         dialect: 'mysql' //gestor de DB
