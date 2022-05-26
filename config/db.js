211e4ad84044d8ba53415a083ba7f565683a6401
root <root@DESKTOP-DMI5NAQ.localdomain>
2022-05-25 22:14:23 -0500

    bd

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ const sequelize = new Sequelize(
     process.env.USER, // Usuario
     process.env.PASSWORD, //Contraseña
     {
-        host: '34.221.160.76', //host
+        host: process.env.HOST, //host
         dialect: 'mysql' //gestor de DB
     });
 
